710de9c3f0992407b622d13229ab7a74f6d6e4ff
crastyle <197203392@qq.com>
2017-05-03 22:26:46 +0800

    bug fix

--- src/pages/carts/carts.js
+++ src/pages/carts/carts.js
@@ -2,7 +2,32 @@ export default {
   name: 'Carts',
   data () {
     return {
-      msg: 'Welcome to Carts'
+      msg: 'Welcome to Carts',
+      carts: [{
+        id: 'fasfwefew',
+        img: '/static/Bitmap.png',
+        name: '冠成非洲花梨 三聚氰胺板 9mm厚芯单面',
+        intro: '芯材：厚芯   纸张：色浆纸   表面处理：浮雕面',
+        price: 190,
+        count: 2,
+        tag: '1'
+      }, {
+        id: 'fasfwefew',
+        img: '/static/Bitmap.png',
+        name: '冠成非洲花梨 三聚氰胺板 9mm厚芯单面',
+        intro: '芯材：厚芯   纸张：色浆纸   表面处理：浮雕面',
+        price: 190,
+        count: 2,
+        tag: '1'
+      }, {
+        id: 'fasfwefew',
+        img: '/static/Bitmap.png',
+        name: '冠成非洲花梨 三聚氰胺板 9mm厚芯单面',
+        intro: '芯材：厚芯   纸张：色浆纸   表面处理：浮雕面',
+        price: 190,
+        count: 2,
+        tag: '1'
+      }]
     }
   }
 }
