e44d93c89594b922565987e1393eb7603a880114
crastyle <197203392@qq.com>
2017-05-05 20:04:47 +0800

    购物车

--- src/pages/carts/carts.js
+++ src/pages/carts/carts.js
@@ -4,30 +4,82 @@ export default {
     return {
       msg: 'Welcome to Carts',
       carts: [{
-        id: 'fasfwefew',
+        id: 'sdf',
         img: '/static/Bitmap.png',
         name: '冠成非洲花梨 三聚氰胺板 9mm厚芯单面',
         intro: '芯材：厚芯   纸张：色浆纸   表面处理：浮雕面',
         price: 190,
-        count: 2,
-        tag: '1'
+        count: 5,
+        tag: '1',
+        isActive: false
       }, {
-        id: 'fasfwefew',
-        img: '/static/Bitmap.png',
+        id: '12ds',
+        img: '/Bitmap.png',
         name: '冠成非洲花梨 三聚氰胺板 9mm厚芯单面',
         intro: '芯材：厚芯   纸张：色浆纸   表面处理：浮雕面',
         price: 190,
-        count: 2,
-        tag: '1'
+        count: 3,
+        tag: '1',
+        isActive: false
       }, {
-        id: 'fasfwefew',
+        id: 'sf132',
         img: '/static/Bitmap.png',
         name: '冠成非洲花梨 三聚氰胺板 9mm厚芯单面',
-        intro: '芯材：厚芯   纸张：色浆纸   表面处理：浮雕面',
-        price: 190,
-        count: 2,
-        tag: '1'
-      }]
+        intro: '芯材：厚芯  纸   表面处理：浮雕面',
+        price: 290,
+        count: 1,
+        tag: '1',
+        isActive: false
+      }],
+
+      //选中的ids
+      chooseIds: [],
+
+      // 全选状态
+      allCheck: false,
+
+      //是否允许结算按钮样式
+      canCalc: false      
+    }
+  },
+  created() {
+   
+  },
+  methods: {
+    choice(item) {
+      console.log(item)
+      if (item.isActive) {
+        item.isActive = false
+        this.chooseIds.splice(item.id, 1)
+      } else {
+        item.isActive = true
+        this.chooseIds.push(item.id)
+      }
+    },
+    allCheckActive() {
+      if (this.allCheck) {
+        this.allCheck = false
+        this.chooseIds = []
+        for (let i = 0; i < this.carts.length; i++) {
+          this.carts[i].isActive = false
+        }
+      } else {
+        this.allCheck = true
+        this.chooseIds = []
+        for (let i = 0; i < this.carts.length; i++) {
+          this.carts[i].isActive = true
+          this.chooseIds.push(this.carts[i]['id'])
+        }
+      }
+    }
+  },
+  watch: {
+    'chooseIds': function(v, o){
+      if (v.length > 0) {
+        this.canCalc = true
+      } else {
+        this.canCalc = false
+      }
     }
   }
 }
